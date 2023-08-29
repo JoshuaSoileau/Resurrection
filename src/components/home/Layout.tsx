6e46cf0111ff071b4402d6b6b6d43cb1725488bb
Joshua Soileau <joshua.soileau@gmail.com>
2023-08-29 19:10:20 -0400

    Removes GA

--- src/components/home/Layout.tsx
+++ src/components/home/Layout.tsx
@@ -11,14 +11,6 @@ interface Props {
 const Layout: NextPage<Props> = ({ children }) => {
   return (
     <div className="min-h-screen bg-gray-50">
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-YD4NZT886Q"></script>
-      <script dangerouslySetInnerHTML={{__html: `
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-YD4NZT886Q');`}} />
-        
       <Header />
       {children}
       <Footer />

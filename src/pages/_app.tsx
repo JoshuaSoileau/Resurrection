602878b5636f9b38bde3e7c6ee3b539000d8db16
Joshua Soileau <joshua.soileau@gmail.com>
2023-09-07 10:52:30 -0400

    Shift props around

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,17 +13,24 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <div tw="scroll-smooth">
       <script
-        id="google-tag-manager"
         dangerouslySetInnerHTML={{
           __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TQRZ5MR6');`,
-        }}
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-TQRZ5MR6');`,
+      }}
+      id="google-tag-manager"
       />
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TQRZ5MR6"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+      <noscript>
+        <iframe 
+          height="0" 
+          src="https://www.googletagmanager.com/ns.html?id=GTM-TQRZ5MR6"
+          style="display:none;visibility:hidden"
+          width="0" 
+          >
+        </iframe>
+        </noscript>
       <GlobalStyles />
       <Component {...pageProps} />
       <Analytics />

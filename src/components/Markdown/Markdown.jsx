ff5fa513b78367d328393ea657a5f2ceb5b1e145
Joshua Soileau <joshua.soileau@gmail.com>
2023-10-08 22:06:44 -0400

    Turn off prop types requirement

--- src/components/Markdown/Markdown.jsx
+++ src/components/Markdown/Markdown.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 
+// eslint-disable-next-line react/prop-types
 export default function Markdown({ content, isAdmin }) {
   return isAdmin ? (
     <ReactMarkdown>{content}</ReactMarkdown>
